27cdceda8c4d00aac4079761c835338529c0c744
shixing <1553872085@qq.com>
2023-02-22 23:52:29 +0800

    新增Python和pytest执行区别

--- testcase/test_mobile_login/test_mobile_login.py
+++ testcase/test_mobile_login/test_mobile_login.py
@@ -3,7 +3,13 @@
 # @Author  : SHIXING
 # @FileName: test_mobile_login.py
 # @Software: PyCharm
+import os
+import sys
+
 import pytest
+
+Base_Dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(Base_Dir)
 
 from api.mobile_login_api import MobileLogin
 from common.wrapper import add_log
@@ -23,3 +29,10 @@ class TestMobileLogin:
     def test_mobile_login(self, case):
         res = self.mobile_login.mobile_login(**case['data'])
         self.mobile_login.check_assert(res, case)
+
+    def test_mobile_login2(self):
+        assert 1 == 2
+
+
+if __name__ == '__main__':
+    pytest.main(['-vs', 'test_mobile_login.py::TestMobileLogin::test_mobile_login2'])
